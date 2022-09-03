dd076850bb11b206cc3c3880e835de87f149623d
Manap <konopyanov.manap@gmail.com>
2022-09-03 12:52:57 +0600

    feat: changed to dark theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import 'antd/dist/antd.css'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import './settings/i18n'
